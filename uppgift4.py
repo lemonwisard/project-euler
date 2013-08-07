087a974c5f508eee4cac1b3f7a7826f90d95ef02
Hugo Laurell <hugolaurell@hugo-laurells-macbook-4.local>
2013-08-07 17:02:02 +0200

    lista över nr

--- uppgift4.py
+++ uppgift4.py
@@ -1,13 +1,11 @@
 
-x = [100,101]
+x = [100,101,1]
 
 for a in range(100,1000,1):
-	xnr=True
-	for j in x[1:]:
-		if xnr==True:
-			x.append(a)
+	x.append(a)
+	print(a)
 z = [100,101]
 for b in range(100,1000,1):
+	z.append(b)
 	print(b)
-		
-print(b*a)
+
