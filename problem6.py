be3173574e0d5f59b0c729c1bbf9c49d48e81a06
Hugo Laurell <hugolaurell@hugo-laurells-macbook-4.local>
2013-08-08 12:42:24 +0200

    hgj

--- problem6.py
+++ problem6.py
@@ -1,15 +1,19 @@
 
 x = [1,2]
 
-for i in range(3,10002,2):
+for i in range(3,50000,2):
 	is_prime=True
 	for j in x[1:]:
 		if i%j==0:
 			is_prime=False
 	if is_prime==True:
 		x.append(i)
+print(x)
 
+tmp = []
 for i in x:
-	x.append(str(i))
-	print(x)
+	tmp.append(str(i))
+print(tmp)
+print(tmp.find(str2,10001));
+
 
